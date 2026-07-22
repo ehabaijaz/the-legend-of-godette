26aedad924fd60490d1b14c4b0ef2d1140fb5dc1
Ehab Aijaz <clutch.10306@gmail.com>
2026-07-22 18:27:38 +0530

    working on the jump logic and behavior

--- camera_controller.gd
+++ camera_controller.gd
@@ -1,17 +1,20 @@
 extends Node3D
 
+@export var min_limit_x:float
+@export var max_limit_x:float
+@export var mouse_acceleration := 0.005	
 func _process(delta: float) -> void:
 	pass
 
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rotate_from_vector(event.relative * 0.005)
+		rotate_from_vector(event.relative * mouse_acceleration)
 
 		
 func rotate_from_vector(v: Vector2):
 	if v.length() == 0: return
-	rotation.y += v.x
+	rotation.y -= v.x
 	rotation.x -= v.y
-	rotation.x = clamp(rotation.x, -0.1, 0.1)
+	rotation.x = clamp(rotation.x, min_limit_x, max_limit_x)
 	
--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -1,5 +1,15 @@
 extends CharacterBody3D
 
+# jump section
+@export var jump_height : float = 2.25
+@export var jump_time_to_peak : float = 0.4
+@export var jump_time_to_descent : float = 0.3
+@onready var jump_velocity : float = ((2.0 * jump_height) /jump_time_to_peak) * -1.0
+@onready var jump_gravity : float = ((-2.0 * jump_height) /(jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity : float = ((-2.0 * jump_height) /(jump_time_to_descent * jump_time_to_descent)) * -1.0
+
+
+
 @export var base_speed := 4.0
 @onready var camera = $CameraController/Camera3D
 
@@ -8,5 +18,13 @@ var movement_input := Vector2.ZERO
 func _physics_process(delta: float) -> void:
 	movement_input = Input.get_vector("left", "right", "forward", "back").rotated(-camera.global_rotation.y)
 	velocity = Vector3(movement_input.x, 0, movement_input.y) * base_speed
+	jump_logic(delta)
+	
 	move_and_slide( )
  
+func jump_logic(delta)->void:
+	if Input.is_action_just_pressed('jump'):
+		velocity.y = 40
+	var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
+	velocity.y -= gravity * delta
+	
